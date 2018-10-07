f77356a41c8d6348c4dac3e5b827bda77543b7e0
indiflex <indiflex1@gmail.com>
2018-10-07 17:09:59 +0900

    brtest

--- src/main/java/com/jade/swp/controller/UserController.java
+++ src/main/java/com/jade/swp/controller/UserController.java
@@ -46,28 +46,13 @@ public class UserController {
 	@Autowired
 	private OAuth2Parameters googleOAuth2Parameters;
 	
-	@RequestMapping(value = "/loginGoogle", method = { RequestMethod.GET, RequestMethod.POST })
-	public String login(Model model, HttpSession session) {
-
-		/* 구글code 발행 */
-		OAuth2Operations oauthOperations = googleConnectionFactory.getOAuthOperations();
-		String url = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, googleOAuth2Parameters);
-
-		System.out.println("구글:" + url);
-
-		model.addAttribute("google_url", url);
-
-		/* 생성한 인증 URL을 View로 전달 */
-		return "login";
-	}
-
 	// 구글 Callback호출 메소드
 	@RequestMapping(value = "/auth/google/callback", method = { RequestMethod.GET, RequestMethod.POST })
 	public String googleCallback(Model model, @RequestParam String code) throws IOException {
 		System.out.println("GGGGGGG>> 여기는 googleCallback: " + code);
 		System.out.println(model.toString());
 
-		return "/board/listPage";
+		return "/googleResult";
 	}
 	
 	// ---------------------------------------------------------------------------------------
